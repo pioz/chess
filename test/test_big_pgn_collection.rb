0c592e0fb54003339a87bbc20e2c200ee1918b2c
pioz <epilotto@gmx.com>
2017-08-10 15:27:49 +0200

    Enable test_big_pgn_collection only if env variable BIG_PGN_COLLECTION is set

--- test/test_big_pgn_collection.rb
+++ test/test_big_pgn_collection.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ChessTest < Minitest::Test
 
-  if File.exist?(TestHelper::BIG_PGN_COLLECTION)
+  if ENV['BIG_PGN_COLLECTION'] && File.exist?(TestHelper::BIG_PGN_COLLECTION)
     puts 'Loading tests for a big PGN collection...'
     300_000.times do |i|
       filename = sprintf("%06d.pgn", i+1)
