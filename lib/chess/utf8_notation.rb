0a2e8f89129203e5d8d8bc1b550ea0559b5e1388
pioz <epilotto@gmx.com>
2017-08-14 19:28:17 +0200

    Convert comments from rdoc to yardoc

--- lib/chess/utf8_notation.rb
+++ lib/chess/utf8_notation.rb
@@ -2,14 +2,15 @@ module Chess
 
   # With this module is possible call the method +to_utf8+ on a string. This
   # method convert the chess piece identifier character into UTF8 chess
-  # character, for example:
-  #     :001 > require 'chess/utf8_notation'
-  #      => true
-  #     :002 > 'Qf7#'.to_utf8
-  #      => '♕f7#'
+  # character.
+  # @example
+  #   :001 > require 'chess/utf8_notation'
+  #   => true
+  #   :002 > 'Qf7#'.to_utf8
+  #   => '♕f7#'
   #
-  # To use this utility explicit require is needed:
-  # <tt>require 'chess/utf8_notation'</tt>
+  # @note To use this utility explicit require is needed: <tt>require
+  #   'chess/utf8_notation'</tt>
   module UTF8Notation
 
     # Map a piece identifier character with the corresponding UTF8 chess
@@ -29,7 +30,8 @@ module Chess
       'k' => '♚'
     }
 
-    # Replace the piece identifier characters with UTF8 chess characters
+    # Replace the piece identifier characters with UTF8 chess characters.
+    # @return [String]
     def to_utf8
       self.gsub(/[PRNBQKprnbqk]/, UTF8_MAP)
     end
