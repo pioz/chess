87c36ad37691d0b28d5d7d38ee07b5f409736b5a
pioz <epilotto@gmx.com>
2024-08-29 17:09:25 +0200

    Clean extconf.rb

--- ext/chess/extconf.rb
+++ ext/chess/extconf.rb
@@ -1,33 +1,8 @@
 # Creating a gem that includes an extension that is built at install time
 # https://guides.rubygems.org/gems-with-extensions/
 
-ENV['RC_ARCHS'] = '' if RUBY_PLATFORM.include?('darwin')
-
 require 'mkmf'
 
-INCLUDE_DIRS = [
-  # Check the ruby install locations
-  RbConfig::CONFIG['includedir'],
-  # First earch /opt/local for macports
-  '/opt/local/include',
-  # Then search /usr/local for people that installed from source
-  '/usr/local/include',
-  # Finally fall back to /usr
-  '/usr/include'
-].freeze
-
-LIB_DIRS = [
-  # Check the ruby install locations
-  RbConfig::CONFIG['libdir'],
-  # First search /opt/local for macports
-  '/opt/local/lib',
-  # Then search /usr/local for people that installed from source
-  '/usr/local/lib',
-  # Finally fall back to /usr
-  '/usr/lib'
-].freeze
-
-# dir_config('chess', INCLUDE_DIRS, LIB_DIRS)
-$CFLAGS << ' -std=c99'
+$CFLAGS += ' -std=c99'
 
 create_makefile('chess/chess')
